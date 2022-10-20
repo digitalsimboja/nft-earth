cf192cba2968bf30b3728edff73294c9c4e6aa9f
Sunday Mgbogu <digitalsimboja@gmail.com>
2022-10-20 09:36:31 +0100

    Fixed wagmi issues

--- pages/_app.js
+++ pages/_app.js
@@ -1,49 +1,74 @@
-import '../styles/globals.css'
-import '@rainbow-me/rainbowkit/styles.css'
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
-import { chain, configureChains, createClient, WagmiConfig } from 'wagmi'
-import { alchemyProvider } from 'wagmi/providers/alchemy'
-import { publicProvider } from 'wagmi/providers/public'
-import { ChakraProvider } from '@chakra-ui/react'
-import { extendTheme } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
+import "@rainbow-me/rainbowkit/styles.css";
+import "../styles/globals.css";
 
-// TODO: Change to EARTH preferred theme
-// Extend the theme to include custom colors, fonts, etc
-const colors = {
-  brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+
+
+const polygonChain = {
+  id: 80001,
+  name: "Mumbai Testnet",
+  network: "polygon",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Matic",
+    symbol: "MATIC",
   },
-}
+  rpcUrls: {
+    default: "  https://rpc-mumbai.maticvigil.com/",
+  },
+  blockExplorers: {
+    default: {
+      name: "polygonscan",
+      url: "https://polygonscan.com/",
+    },
+  },
+  testnet: true,
+};
 
-const theme = extendTheme({ colors })
-
-// Configure the chains
 const { chains, provider } = configureChains(
-  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
-  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
-)
+  [polygonChain],
+  [
+    jsonRpcProvider({
+      rpc: (chain) => {
+        if (chain.id !== polygonChain.id) {
+          throw new Error("Error! Switch your network to Polygon Mainnnet");
+          return null;
+        };
+        return { http: chain.rpcUrls.default };
+      },
+    }),
+  ]
+);
+
 const { connectors } = getDefaultWallets({
-  appName: 'E(AR)TH App',
+  appName: "Eth NFTMarketplace",
   chains,
-})
+});
+
 const wagmiClient = createClient({
   autoConnect: true,
   connectors,
   provider,
-})
+});
+
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
-      <WagmiConfig client={wagmiClient}>
-        <RainbowKitProvider chains={chains}>
+
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+
+        <ChakraProvider>
           <Component {...pageProps} />
-        </RainbowKitProvider>
-      </WagmiConfig>
-    </ChakraProvider>
-  )
+        </ChakraProvider>
+
+      </RainbowKitProvider>
+    </WagmiConfig>
+
+  );
 }
 
-export default MyApp
+export default MyApp;
