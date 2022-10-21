cdeba89ec543714b429c5be7f3d240c9b8ecb5dc
Sunday Mgbogu <digitalsimboja@gmail.com>
2022-10-21 16:47:25 +0100

    map display fixed

--- pages/components/Map.js
+++ pages/components/Map.js
@@ -1,28 +1,26 @@
+import React, { useState, useEffect } from "react";
+import { MapContainer, Circle, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
-import { useState, useEffect } from 'react';
-import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet';
+function Map() {
+  const position = [51.505, -0.09]
+   const fillBlueOptions = { fillColor: "#0484D6" };
+   const [map, setMap] = useState(null);
 
-export function ChangeView({ coords }) {
-  const map = useMap();
-  map.setView(coords, 12);
-  return null;
+   useEffect(() => {
+      if (map) {
+         setInterval(function () {
+            map.invalidateSize();
+         }, 100);
+      }
+   }, [map]);
+
+   return (
+      <MapContainer center={position} zoom={20} scrollWheelZoom={false} style={{ height: "400px", width: "100%" }} whenCreated={setMap}>
+         <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+         <Circle center={position} pathOptions={fillBlueOptions} radius={50} />
+      </MapContainer>
+   );
 }
 
-export default function Map() {
-  const [geoData, setGeoData] = useState({ lat: 64.536634, lng: 16.779852 })
-
-  const center = [geoData.lat, geoData.lng]
-  return (
-    <MapContainer center={center} zoom={12} style={{ height: '100vh' }}>
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {geoData.lat && geoData.lng && (
-        <Marker position={[geoData.lat, geoData.lng]} />
-      )}
-      <ChangeView coords={center} />
-    </MapContainer>
-  );
-}
+export default Map;
--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,21 @@
-import { useEffect, useState } from "react";
-import dynamic from "next/dynamic"
-import { useRouter } from "next/router";
-import { useAccount } from "wagmi";
+import React from "react";
+import dynamic from "next/dynamic";
 import { Box, useColorModeValue } from "@chakra-ui/react";
-import Navbar from "./components/Navbar";
-import SideMenu from "./components/SideMenu";
-import Header from "./components/Header";
-import Head from 'next/head';
-
-
-import styles from '../styles/Home.module.css'
-
-const EarthMap = dynamic(() => import("./components/Map"), { ssr: false })
-const DEFAULT_CENTER = [38.907132, -77.036546]
+import  Navbar from './components/Navbar'
 
 export default function Home() {
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+  const MapWithNoSSR = dynamic(() => import("./components/Map"), {
+    ssr: false
+  });
 
   return (
+    
     <Box width={"100vw"}  >
       {/* Add the Navbar */}
       <Navbar bg={useColorModeValue("white", "gray.800")} />
-      <div className={styles.container}>
-        <Head>
-          <title>Earth Map</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-
-        <main className={styles.main}>
-          <h1 className={styles.title}>
-            Earth NFT Map
-          </h1>
-          <EarthMap className={styles.homeMap} center={DEFAULT_CENTER} zoom={12}>
-     
-          </EarthMap>
-
-
-        </main>
-
-        <footer className={styles.footer}>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by{' '}
-            <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-          </a>
-        </footer>
+      <div id="map">
+        <MapWithNoSSR />
       </div>
     </Box>
-
-  )
+  );
 }
