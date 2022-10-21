d5f2eb4bf45ea4679b7cc5ea01d5302ac1c40af9
Sunday Mgbogu <digitalsimboja@gmail.com>
2022-10-21 17:28:58 +0100

    Increased map display window

--- pages/components/Map.js
+++ pages/components/Map.js
@@ -3,7 +3,7 @@ import { MapContainer, Circle, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 function Map() {
-  const position = [51.505, -0.09]
+   const position = [51.505, -0.09]
    const fillBlueOptions = { fillColor: "#0484D6" };
    const [map, setMap] = useState(null);
 
@@ -16,7 +16,18 @@ function Map() {
    }, [map]);
 
    return (
-      <MapContainer center={position} zoom={20} scrollWheelZoom={false} style={{ height: "400px", width: "100%" }} whenCreated={setMap}>
+      <MapContainer  center={position}
+      zoom={20}
+      doubleClickZoom={true}
+      scrollWheelZoom={true}
+      dragging={true}
+      animate={true}
+      style={{
+        height: "900px",
+        width: "100%",
+        position: "absolute",
+        zIndex: "1",
+       }} whenCreated={setMap}>
          <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
          <Circle center={position} pathOptions={fillBlueOptions} radius={50} />
       </MapContainer>
